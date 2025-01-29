26d3d0d0085f38b7f7c7cc7f7e9442b266422df6
Troyrodz <troy.rodriguez02@utrgv.edu>
2025-01-29 05:08:27 +0000

    Did I do this right?

---
+++ src/MixMatch.java
@@ -0,0 +1,35 @@
+public class MixMatch{
+    public static void main(String[] args){
+
+        String word = "Possum";
+        System.out.println(word + " <- Strings have to be declared with a capital S.");
+
+        char letter = 'A'; 
+        System.out.println(letter + " <- A char takes up 2 bytes or 16 bits of memory.");
+
+        short idk = 50;
+        System.out.println(idk + " <- When a short is not initialized, it will automatically set to 0.");
+
+        int myInt = 10;
+        System.out.println(myInt + " <- Int is like a short but it 4 byes instead of a shorts 2 bytes.");
+
+        long myLong = 50000000;
+        System.out.println(myLong + " <- A long also stores a number but this one holds larger numbers than in and short. 8 bytes.");
+
+        float myFloat = 5.34f;
+        System.out.println(myFloat + " <- When using initializing a float you should probably use an f after typing the number.");
+
+        double myDouble = 3.4566;
+        System.out.println(myDouble + " <- Double is like a float but allows for bigger numbers (8 bytes).");
+
+        boolean assemblySucks = true;
+        System.out.println(assemblySucks + " <- Strictly used for true and false.");
+
+
+
+
+
+
+
+    }
+}
